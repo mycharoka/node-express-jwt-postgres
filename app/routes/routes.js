023555cf0dd3943d75bd237a43e9abbc285c019d
Muhammad Vidi <muhammadvidi.m@gmail.com>
2020-09-02 13:57:28 +0700

    [UPDATE] endpoint changes

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,27 +1,76 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../service/service')
+const express = require('express');
+const router = express.Router();
 const {
     authToken
-} = require('../config/middleware')
-
+} = require('../config/middleware');
+const {
+    findAllRegisteredUser,
+    findUserById,
+    createSignUp,
+    userLogin,
+    updatePassword,
+    deleteUser
+} = require('../service/service');
+const {
+    response
+} = require('express');
+// const User = require('../service/service');
 
 // buat user baru
-router.post('/', User.createSignUp)
+// router.post('/register', User.createSignUp);
+router.post('/register', async (request, response) => {
+    const {
+        username,
+        password
+    } = request.body
+    let result = await createSignUp(username, password)
+    response.json(result)
+});
 
 // cari user 1 aja
-router.get('/auth', authToken, User.findUserById)
+// router.get('/auth', authToken, User.findUserById);
+router.get('/auth', authToken, async (request, response) => {
+    const result = await findUserById(request.user.id)
+    response.json(result)
+});
 
 // cari semua user
-router.get('/', User.findAllRegisteredUser)
+router.get('/', async (request, response) => {
+    let result = await findAllRegisteredUser();
+    console.log('result routes >> ', result)
+    response.json(result);
+});
 
 // user login
-router.post('/login', User.userLogin)
+// router.post('/login', User.userLogin);
+router.post('/auth', async (request, response) => {
+    const {
+        username,
+        password
+    } = request.body
+    let result = await userLogin(username, password)
+    response.json(result)
+});
 
 // delete user
-router.delete('/delete', authToken, User.deleteUser)
+// router.delete('/delete', authToken, User.deleteUser);
+router.delete('/delete', authToken, async (request, response) => {
+    const username = request.user.username
+    const password = request.body.password
+    let result = await deleteUser(username, password)
+    response.json(result)
+});
 
 // update user
-router.put('/update', authToken, User.updatePassword)
+// router.put('/update', authToken, User.updatePassword);
+router.put('/update', authToken, async (request, response) => {
+    const username = request.user.username
+    const {
+        password,
+        newPassword
+    } = request.body
+    let result = await updatePassword(username, password, newPassword)
+    response.json(result)
+});
 
-module.exports = router
+module.exports = router;
--- app/service/service.js
+++ app/service/service.js
@@ -1,90 +1,86 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const User = require('../models/model')
-
+const Models = require('../models/model');
 
 // TODO: CREATE USER
-const createSignUp = async (request, response) => {
-    const {
-        username,
-        password
-    } = request.body;
+const createSignUp = async (username, password) => {
+    // const {
+    //     username,
+    //     password
+    // } = request.body;
 
     try {
-
-        if ((username == "" || username.trim().length == 0) || (password == "" || password.trim().length == 0)) {
-            return response.status(422).json({
-                message: "USERNAME / PASSWORD GABOLEH KOSONG"
-            })
+        if (username == '' || username.trim().length == 0 || (password == '' || password.trim().length == 0)) {
+            return {
+                message: 'USERNAME / PASSWORD GABOLEH KOSONG'
+            };
         }
-        const findUserResult = await User.findUserData(username);
-
+        const findUserResult = await Models.findUserData(username);
 
         if (!findUserResult) {
-
             const salt = await bcrypt.genSalt(10);
             const hashedPassword = await bcrypt.hash(password, salt);
-            const insertDataResult = await User.insertUserData(username, hashedPassword);
-
-            return response.json({
+            const insertDataResult = await Models.insertUserData(username, hashedPassword);
+            return {
                 message: `${username} BERHASIL TERDAFTAR`
-            });
+            };
         }
-        return response.status(401).json({
+        return {
             message: `${username} DUPLIKAT`
-        });
-
+        };
     } catch (error) {
-        return response.status(404).json({
-            message: "error"
-        })
+        return {
+            status: 'error',
+            message: error.message
+        };
     }
 };
 
 // TODO: FIND USER BY ID
-const findUserById = async (request, response) => {
-    const id = request.user.id;
+const findUserById = async (id) => {
+    // const id = request.user.id;
 
     try {
-        const findUserIdResult = await User.findUserId(id);
-        return response.json({
+        const findUserIdResult = await Models.findUserId(id);
+        return {
             message: `${id} ini terdaftar`,
             data: findUserIdResult
-        });
-
+        };
     } catch (error) {
-        return response.status(404).json({
-            message: 'ID NOT FOUND'
-        });
-
+        return {
+            status: "error",
+            message: error.message
+        };
     }
 };
 
 // TODO: FIND ALL USER
-const findAllRegisteredUser = async (request, response) => {
+const findAllRegisteredUser = async () => {
     try {
-        const getAllUsersResult = await User.findRegisteredUser();
-        response.json({
+        const getAllUsersResult = await Models.findRegisteredUser();
+        console.log('cek all >> ', getAllUsersResult)
+        return {
             message: 'Get ALL!',
             data: {
                 getAllUsersResult
             }
-        });
+        };
     } catch (error) {
-        response.status(404).json({
-            message: 'FIND ALL ONLY'
-        })
+        return {
+            status: "error",
+            message: error.message
+        };
     }
 };
 
 // TODO: USER LOGIN
 
-const userLogin = async (request, response) => {
-    const username = request.body.username;
-    const password = request.body.password;
+const userLogin = async (username, password) => {
+    // const username = request.body.username;
+    // const password = request.body.password;
 
     try {
-        const userResult = await User.findUserData(username)
+        const userResult = await Models.findUserData(username);
         const comparePassword = await bcrypt.compare(password, userResult.password); // <== compare password
 
         // TODO: buat generate token JWT
@@ -98,101 +94,97 @@ const userLogin = async (request, response) => {
             const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
                 expiresIn: '1h'
             });
-            response.json({
+            return {
                 token: accessToken,
                 data: {
                     id: userResult.id,
                     username: userResult.username
                 }
-            });
-
+            };
         } else {
-            response.status(404).json({
+            return {
                 message: 'AKUN BLM ADA / SALAH MASUKKIN USERNAME DAN PASSWORD'
-            });
+            };
         }
-
-
     } catch (error) {
-        response.status(404).json({
-            message: "COBA CEK LAGI USERNAME SAMA PASSWORDNYA BRAY"
-        })
+        return {
+            status: 'COBA CEK LAGI USERNAME SAMA PASSWORDNYA BRAY',
+            message: error.message
+        };
     }
 };
 
 //TODO: DELETE USSER
 
-const deleteUser = async (request, response) => {
-    const username = request.user.username
-    const {
-        password
-    } = request.body
-
+const deleteUser = async (username, password) => {
+    // const username = request.user.username;
+    // const {
+    //     password
+    // } = request.body;
 
     try {
-
-        const findUserResult = await User.findUserData(username)
-        const comparePassword = await bcrypt.compare(password, findUserResult.password)
+        const findUserResult = await Models.findUserData(username);
+        const comparePassword = await bcrypt.compare(password, findUserResult.password);
         if (findUserResult) {
             if (comparePassword) {
-                const deleteUser = await User.deleteUserData(username)
-                response.json({
+                const deleteUser = await Models.deleteUserData(username);
+                return {
                     message: `${username} DELETED`,
                     data: {
                         id: findUserResult.id,
                         username: findUserResult.username
                     }
-                })
+                };
             } else {
-                response.status(401).json({
-                    message: `NOT DELETED PROPERLY`
-                })
+                return {
+                    message: `TERJADI KESALAHAN !! COBA CEK PASSWORD`
+                };
             }
         }
     } catch (error) {
-        response.status(404).json({
-            message: 'DELETING PROCESS ERROR'
-        })
+        return {
+            status: 'DELETING PROCESS ERROR',
+            message: error.message
+        };
     }
-}
+};
 
 // TODO: UPDATE PASSWORD
-const updatePassword = async (request, response) => {
-    const username = request.user.username
-    const password = request.body.password
-    const newPassword = request.body.newPassword
+const updatePassword = async (username, password, newPassword) => {
+    // const username = request.user.username;
+    // const password = request.body.password;
+    // const newPassword = request.body.newPassword;
 
     try {
-        if (newPassword == "" || newPassword.trim().length == 0) {
-            return response.status(401).json({
-                message: "PASSWORD BARU TIDAK BOLEH KOSONG"
-            })
+        if (newPassword == '' || newPassword.trim().length == 0) {
+            return {
+                message: 'PASSWORD BARU TIDAK BOLEH KOSONG'
+            };
         }
 
-        const checkUsernameResult = await User.findUserData(username)
-        const comparePassword = await bcrypt.compare(password, checkUsernameResult.password)
+        const checkUsernameResult = await Models.findUserData(username);
+        const comparePassword = await bcrypt.compare(password, checkUsernameResult.password);
 
         // TODO: compare password yg dimasukkin dari body sama yg di database..
         if (comparePassword) {
             const salt = await bcrypt.genSalt(10);
             const hashedPassword = await bcrypt.hash(newPassword, salt);
-            const updatePassword = await User.updateUserPassword(hashedPassword, username)
-            response.json({
+            const updatePassword = await Models.updateUserPassword(hashedPassword, username);
+            return {
                 message: 'PASSWORD TELAH TERGANTI'
-            })
-
+            };
         } else {
-            response.status(401).json({
+            return {
                 message: 'PASSWORD TIDAK COCOK'
-            })
+            };
         }
-
     } catch (error) {
-        response.status(404).json({
-            message: 'ADA KESALAHAN COBA CEK LAGI'
-        })
+        return {
+            status: 'ADA KESALAHAN COBA CEK LAGI',
+            message: error.message
+        };
     }
-}
+};
 
 module.exports = {
     createSignUp,
