453a5a1e9b4e952997b8cfea2ea0210ad59ca5f8
Muhammad Vidi <muhammadvidi.m@gmail.com>
2020-09-04 15:25:22 +0700

    [UPDATE]jwt middleware function to detect role

--- app/config/middleware.js
+++ app/config/middleware.js
@@ -1,40 +1,39 @@
 const jwt = require('jsonwebtoken')
 
-const authToken = (request, response, next) => {
-    try {
-        const authHeader = request.headers.authorization
-        console.log('if exist => ', authHeader)
-        if (!authHeader) {
-            console.log('cek header => ', authHeader)
-            return response.status(401).json({
-                message: 'Empty Headers..check it first'
+const authToken = (...role) => {
+    return (request, response, next) => {
+        try {
+            const authHeader = request.headers.authorization
+            console.log('if exist => ', authHeader)
+            if (!authHeader) {
+                console.log('cek header => ', authHeader)
+                return response.status(401).json({
+                    message: 'Empty Headers..check it first'
+                })
+            } else {
+                console.log('header cek => ', authHeader)
+                const token = authHeader.split(' ')[1]
+                if (!token) return response.status(401)
+                const verifyToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+                console.log('isi verify >> ', verifyToken)
+                console.log(' role >> ', role)
+                // console.log('isi verify role >> ', verifyToken.user.role['admin'])
+                if (!role.includes(verifyToken.user.role)) {
+                    return response.json({
+                        status: "error",
+                        message: "role not authorized"
+                    })
+                }
+                request.user = verifyToken.user
+                console.log('request user proses middleware >> ', request.user)
+                next()
+            }
+
+        } catch (error) {
+            response.status(404).json({
+                message: error.message
             })
-        } else {
-            console.log('header cek => ', authHeader)
-            const token = authHeader.split(' ')[1]
-            if (!token) return response.status(401)
-            // const verifyToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, userData) => {
-            //     if (err) {
-            //         return response.status(403)
-            //     }
-            //     request.userData = userData
-            //     next()
-            // })
-            const verifyToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-            request.user = verifyToken.user
-            console.log('request user proses middleware >> ', request.user)
-
-            // response.status(200).json({
-            //     message: request.userData.user
-            // })
-            next()
-            // console.log('verify token => ', verifyToken)
         }
-
-    } catch (error) {
-        response.status(404).json({
-            message: 'TOKEN NOT FOUND'
-        })
     }
 }
 
