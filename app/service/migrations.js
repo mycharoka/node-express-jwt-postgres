5dd1dd7a2a2365095f34d7c5cc3c35b3c7408dc0
Muhammad Vidi <muhammadvidi.m@gmail.com>
2020-09-04 15:27:59 +0700

    [ADD]add migration function

--- app/service/migrations.js
+++ app/service/migrations.js
@@ -3,14 +3,14 @@ const bcrypt = require('bcrypt')
 let migration = async (client) => {
     try {
         await client.query(
-            "CREATE TABLE IF NOT EXISTS latihan( id serial PRIMARY KEY, username VARCHAR UNIQUE NOT NULL, password VARCHAR NOT NULL)"
+            "CREATE TABLE IF NOT EXISTS training( id serial PRIMARY KEY, username VARCHAR UNIQUE NOT NULL, password VARCHAR NOT NULL, role VARCHAR NOT NULL)"
         )
 
         const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(process.env.PASSWORD, salt)
 
-        let firstUserQuery = "INSERT INTO latihan (username, password) VALUES($1, $2)"
-        let firstUserValue = [process.env.USERNAME_TISU, hashedPassword]
+        let firstUserQuery = "INSERT INTO training (username, password, role) VALUES($1, $2, $3)"
+        let firstUserValue = [process.env.USERNAME_TISU, hashedPassword, "admin"]
         let response = await client.query(firstUserQuery, firstUserValue)
         if (response) {
             console.info("INITIALIZE FIRST USER AND PASSWORD SUCCESS!!")
